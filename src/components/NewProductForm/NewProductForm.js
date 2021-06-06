ccbf6f415e4022b9a1feba2e132aa800bfb440eb
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-06-06 22:41:38 +0200

    formik

--- src/components/NewProductForm/NewProductForm.js
+++ src/components/NewProductForm/NewProductForm.js
@@ -87,7 +87,95 @@ function NewProductForm({ toggleNewProductForm, saveNewProduct }) {
                 hasErrorMessage={touched.title}
                 errorMessage={errors.title}
               />
-              {/* the other fields... */}
+              <Input
+                type="number"
+                label="Price"
+                id="price"
+                value={values.price}
+                placeholder="Price of product"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.price}
+                errorMessage={errors.price}
+              />
+              <Input
+                type="image"
+                label="Upload an image"
+                id="image"
+                value={values.img}
+                placeholder="Upload an image"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.img}
+                errorMessage={errors.img}
+              />
+              <Input
+                type="text"
+                label="Short description"
+                id="shortDescription"
+                value={values.shortDescription}
+                placeholder="Write a short description"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.shortDescription}
+                errorMessage={errors.shortDescription}
+              />
+              <Input
+                type="text"
+                label="Long description"
+                id="longDescription"
+                value={values.longDescription}
+                placeholder="Product title"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.longDescription}
+                errorMessage={errors.longDescription}
+              />
+              <Input
+                type="numbers"
+                label="Units in stock"
+                id="unitsInStock"
+                value={values.unitsInStock}
+                placeholder="How many units in stock?"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.unitsInStock}
+                errorMessage={errors.unitsInStock}
+              />
+              <Input
+                type="text"
+                label="First name"
+                id="authorFirstName"
+                value={values.authorFirstName}
+                placeholder="Write your firts name"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.authorFirstName}
+                errorMessage={errors.authorFirstName}
+              />
+              <Input
+                type="text"
+                label="Last name"
+                id="authorLastName"
+                value={values.authorLastName}
+                placeholder="Write your last name"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.authorLastName}
+                errorMessage={errors.authorLastName}
+              />
+              <Input
+                type="email"
+                label="email"
+                id="authorEmail"
+                value={values.authorEmail}
+                placeholder="Witch is your email"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.authorEmail}
+                errorMessage={errors.authorEmail}
+              />
+
               <Button submitButton block disabled={isValidating || !isValid}>
                 Submit
               </Button>
--- src/components/NewProductForm/product-schema.js
+++ src/components/NewProductForm/product-schema.js
@@ -1,15 +1,36 @@
 import * as Yup from "yup";
 
 const productSchema = Yup.object().shape({
-  title: Yup.string().required("The title is required"),
-  price: Yup.number().required("The price is required"),
-  img: Yup.string().required("The image url is required"),
-  shortDescription: Yup.string().required("The short description is required"),
-  longDescription: Yup.string().required("The long description is required"),
-  unitsInStock: Yup.number().required("The units in stock is required"),
-  authorFirstName: Yup.string().required("The author first name is required"),
-  authorLastName: Yup.string().required("The author last name is required"),
-  authorEmail: Yup.string().required("Required"),
+  title: Yup.string()
+    .required("The title is required")
+    .min(2, "The title is too short!")
+    .max(2, "The title is too long!"),
+  price: Yup.number()
+    .required("The price is required")
+    .integer("the price must be an integer")
+    .positive("the price must be positive"),
+  img: Yup.string()
+    .required("The image url is required")
+    .url("The image url is invalid"),
+  shortDescription: Yup.string()
+    .required("The short description is required")
+    .min(2, "The short description is too short")
+    .max(50, "The short description is too long"),
+  longDescription: Yup.string()
+    .required("The long description is required")
+    .min(2, "The long description is too short")
+    .max(50, "The long description is too long"),
+  unitsInStock: Yup.number()
+    .required("The units in stock is required")
+    .integer("The units in stock must be an integrer")
+    .positive("The units in stock must be a positive number"),
+  authorFirstName: Yup.string()
+    .required("The author first name is required")
+    .min("The author first name is too short"),
+  authorLastName: Yup.string()
+    .required("The author last name is required")
+    .min(2, "The author lastname is too short"),
+  authorEmail: Yup.string().required("Required").email("Invalid email"),
 });
 
 export default productSchema;
